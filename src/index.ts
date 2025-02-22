f0e5dd117bceed1f1451d5452674bdecbee284c3
BianchiBill <rafaelbianchi06@gmail.com>
2025-02-21 23:50:59 -0300

    chore: update version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { log, logError, logWarning } from './utils/log.ts';
 
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .argument('<directory>', 'Diretório para escanear')
   .option('-d, --delete', 'Remover duplicatas automaticamente')
   .action(async (options) => {
